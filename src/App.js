4c1bf422303c08c5c7c797d58a1568e70987e246
HaNguyenCS <nguyenthanhha190903@gmail.com>
2025-06-15 15:57:34 -0400

    comment out auth

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header/header.tsx';
 import Menu from './pages/Menu/menu.tsx';
 import { CartProvider } from './context/cartContext.tsx';
 import { Authenticator } from '@aws-amplify/ui-react';
-import AdminPage from './pages/Admin/admin';
+// import AdminPage from './pages/Admin/admin';
 
 const App = () => {
   return (
@@ -21,14 +21,14 @@ const App = () => {
               <Route path="/about" element={<div>About Page Coming Soon</div>} />
               <Route path="/contact" element={<div>Contact Page Coming Soon</div>} />
               <Route path="/cart" element={<div>404 - Page Not Found</div>} />
-              <Route 
+              {/* <Route 
                   path="/admin" 
                   element={
                     <Authenticator>
                       <AdminPage />
                     </Authenticator>
                   } 
-              />
+              /> */}
             </Routes>
           </main>
         {/* <Footer /> */}
